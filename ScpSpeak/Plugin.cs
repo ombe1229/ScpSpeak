12411d93a35a72f23bb6707f83ce331d8387094a
ombe <h3236516@gmail.com>
2021-05-05 07:59:40 +0900

    scpspeak

--- ScpSpeak/Plugin.cs
+++ ScpSpeak/Plugin.cs
@@ -1,28 +1,32 @@
-﻿using Features = Exiled.API.Features;
+﻿using System;
+using HarmonyLib;
+using Features = Exiled.API.Features;
 using Log = Exiled.API.Features.Log;
 
 namespace ScpSpeak
 {
     public class ScpSpeak : Features.Plugin<Configs>
     {
-        private EventHandlers EventHandlers { get; set; }
-
-        private void LoadEvents()
-        {
-            
-        }
+        public Harmony Harmony { get; private set;  }
 
         public override void OnEnabled()
         {
             if (!Config.IsEnabled) return;
             Log.Info("Plugin is enabled.");
-            EventHandlers = new EventHandlers(this);
-            LoadEvents();
+            try
+            {
+                Harmony = new Harmony($"com.ombe1229.ScpSpeak.{DateTime.Now.Ticks}");
+                Harmony.PatchAll();
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Patching failed: {e.ToString()}");
+            }
         }
 
         public override void OnDisabled()
         {
-            
+            Harmony.UnpatchAll();
         }
     }
 }
