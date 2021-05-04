34d9520e0f9ead349d9d215f80babff35f095fe7
ombe <h3236516@gmail.com>
2021-05-05 07:59:00 +0900

    patch

--- ScpSpeak/Patches/Speak.cs
+++ ScpSpeak/Patches/Speak.cs
@@ -1,14 +1,27 @@
-﻿using Assets._Scripts.Dissonance;
+﻿using System.Collections.Generic;
+using Assets._Scripts.Dissonance;
 using HarmonyLib;
+using System.Linq;
 
 namespace ScpSpeak.Patches
 {
     [HarmonyPatch(typeof(DissonanceUserSetup), nameof(DissonanceUserSetup.CallCmdAltIsActive))]
-    public class Speak
+    public class ScpSpeak
     {
-        public static bool Prefix(DissonanceUserSetup __player, bool value)
+        public static void Prefix(DissonanceUserSetup __instance, bool value)
         {
-            CharacterClassManager classManager = __player.gameObject
+            CharacterClassManager ccm = __instance.gameObject.GetComponent<CharacterClassManager>();
+            if (IsScp(ccm.CurClass)) __instance.MimicAs939 = value;
+        }
+
+        private static bool IsScp(RoleType role)
+        {
+            List<RoleType> scpList = new List<RoleType>
+            {
+                RoleType.Scp049, RoleType.Scp079, RoleType.Scp096, RoleType.Scp106, RoleType.Scp173, RoleType.Scp0492,
+                RoleType.Scp93953, RoleType.Scp93989
+            };
+            return scpList.Contains(role);
         }
     }
 }
